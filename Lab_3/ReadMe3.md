e4e916265f0f1fbc0596d78c7aa72978f0f44dac
Dean Coco <cocodean.a@gmail.com>
2017-02-07 13:48:10 -0800

    removed all old files for new files

--- Lab_3/ReadMe3.md
+++
@@ -1,1 +0,0 @@
-This lab will incorporate the modules defined in Lab 1 and Lab 2. The goal of this lab is to implement a digital to analog converter.
--- Lab_3/SPI_DAC_interface_424.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xelab  -wto e2b009f0aedb46d2b71bab24592291bd -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot SPI_Interface_test_behav xil_defaultlib.SPI_Interface_test -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
